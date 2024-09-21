f65345e216511a0a40acf3f6eca47c640a067626
aibn-ich <aibn-ich@c3r1p10.1337.ma>
2024-09-21 02:02:31 +0100

    UP

--- Exec/error_handle.c
+++ Exec/error_handle.c
@@ -6,13 +6,13 @@
 /*   By: aibn-ich <aibn-ich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 01:39:52 by aibn-ich          #+#    #+#             */
-/*   Updated: 2024/09/21 01:48:45 by aibn-ich         ###   ########.fr       */
+/*   Updated: 2024/09/21 02:02:26 by aibn-ich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void    print_error(char *s1, char *s2, int value)
+void    print_error(char *s1, char *s2, char *s3, int value)
 {
     int saved_fd;
     
@@ -22,5 +22,7 @@ void    print_error(char *s1, char *s2, int value)
         printf("%s\n", s1);
     else if (value == 1)
         printf("%s: %s\n", s1, s2);
+    else if (value == 2)
+        printf("%s: %s: %s\n", s1, s2, s3);
     dup2(saved_fd, STDERR_FILENO);
 }
