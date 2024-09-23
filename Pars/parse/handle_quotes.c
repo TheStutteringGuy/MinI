7ba1eeee7c7a2b400bf63e3961d5f7456ad12148
kinpashi <a.ahlaqqach@gmail.com>
2024-09-23 21:10:37 +0100

    ahmed

--- Pars/parse/handle_quotes.c
+++ Pars/parse/handle_quotes.c
@@ -6,14 +6,13 @@
 /*   By: ahmed <ahmed@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 01:20:18 by aahlaqqa          #+#    #+#             */
-/*   Updated: 2024/09/23 20:48:45 by ahmed            ###   ########.fr       */
+/*   Updated: 2024/09/23 21:09:35 by ahmed            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
 // Function to handle double quotes
-
 char *remove_quotes(char *token, t_linked *env_list)
 {
     size_t len;
@@ -26,6 +25,7 @@ char *remove_quotes(char *token, t_linked *env_list)
     char *env_value;
     char *str;
     size_t env_len;
+    size_t str_len;
     char *temp;
 
     len = ft_strlen(token);
@@ -50,49 +50,75 @@ char *remove_quotes(char *token, t_linked *env_list)
             current_quote = '\0';
             i++;
         }
-        else if (token[i] == '$' && current_quote != '\'')
+        else if (token[i] == '$')
         {
-            i++;
-            env_start = i;
-            while (i < len && (ft_isalnum(token[i]) || token[i] == '_' || token[i] == '?'))
+            if (current_quote != '\'')
+            {
+                if (i + 1 < len && (ft_isalnum(token[i + 1]) || token[i + 1] == '_'))
+                {
+                    i++;
+                    env_start = i;
+                    while (i < len && (ft_isalnum(token[i]) || token[i] == '_' || token[i] == '?'))
+                        i++;
+                    env_var = ft_substr(token, env_start, i - env_start);
+                    if (ft_isdigit(env_var[0]))
+                    {
+                        return (ft_strdup(env_var + 1));
+                    }
+                    if (env_var[0] == '?' && env_var[1] == '\0')
+                    {
+                        free(env_var);
+                        return (ft_itoa(last_exit_status));
+                    }
+                    else if (env_var[0] == '?' && env_var[1] != '\0')
+                    {
+                        str = ft_itoa(last_exit_status);
+                        env_var[0] = str[0];
+                        return (env_var);
+                    }
+                    env_value = ft_getenv(env_list, env_var);
+                    free(env_var);
+                    if (env_value == NULL)
+                    {
+                        free(new_token);
+                        return (NULL);
+                    }
+                    env_len = ft_strlen(env_value);
+                    temp = malloc(j + env_len + 1);
+                    if (!temp)
+                    {
+                        printf("Error: malloc failed\n");
+                        exit(1);
+                    }
+                    if (j > 0)
+                    {
+                        ft_memcpy(temp, new_token, j);
+                    }
+                    ft_memcpy(&temp[j], env_value, env_len);
+                    j += env_len;
+                    temp[j] = '\0';
+                    free(new_token);
+                    new_token = temp;
+                }
+                else if (i + 1 < len && token[i + 1] == '?')
+                {
+                    i += 2;
+                    str = ft_itoa(last_exit_status);
+                    str_len = ft_strlen(str);
+                    ft_memcpy(&new_token[j], str, str_len);
+                    j += str_len;
+                    free(str);
+                }
+                else
+                {
+                    new_token[j++] = '$';
+                    i++;
+                }
+            }
+            else
+            {
+                new_token[j++] = '$';
                 i++;
-            env_var = ft_substr(token, env_start, i - env_start);
-            if (ft_isdigit(env_var[0]))
-                return (ft_strdup(env_var + 1));
-            if (env_var[0] == '?' && env_var[1] == '\0')
-                return (ft_itoa(last_exit_status));
-            else if (env_var[0] == '?' && env_var[1] != '\0')
-            {
-                str = ft_itoa(last_exit_status);
-                env_var[0] = str[0];
-                return (env_var);
-            }
-            env_value = ft_getenv(env_list, env_var);
-            if (env_value == NULL)
-            {
-                free(env_var);
-                free(new_token);
-                return (NULL);
-            }
-            free(env_var);
-            if (env_value)
-            {
-                env_len = ft_strlen(env_value);
-                temp = malloc(j + env_len + 1);
-                if (!temp)
-                {
-                    printf("Error: malloc failed\n");
-                    exit(1);
-                }
-                if (j > 0)
-                {
-                    ft_memcpy(temp, new_token, j);
-                }
-                ft_memcpy(&temp[j], env_value, env_len);
-                j += env_len;
-                temp[j] = '\0';
-                free(new_token);
-                new_token = temp;
             }
         }
         else if (token[i] == '\\' && current_quote == '"')
